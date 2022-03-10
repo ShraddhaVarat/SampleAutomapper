8bc10c168027124479f92c37ff8b4103634ea1bf
ShraddhaVarat <32396497+ShraddhaVarat@users.noreply.github.com>
2022-03-11 00:21:24 +0530

    Add files via upload

---
+++ EmployeeAutomapper/Shared/EmployeeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAutomapper.Shared
+{
+    public enum EmployeeType
+    {
+        Employee = 0,
+        Manager = 1,
+    }
+}
